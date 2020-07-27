9432dd83b15790dfa1c3988401dcb8768a263d93
João Gabriel Dourado Cervo <joaogabrieldouradocervo@gmail.com>
2020-07-27 09:32:39 -0300

    Removido param do request

--- routes/main.js
+++ routes/main.js
@@ -1,11 +1,8 @@
-const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 const ideaController = require('../controllers/ideas.js');
 
-router.get('/', (req, res) => {
-    // console.log(req.query[0]);
-    
+router.get('/', (req, res) => {    
     ideaController.getIdeas(req, res, function(error, idea){
         if(error) {
             console.log("ih deu merda");
